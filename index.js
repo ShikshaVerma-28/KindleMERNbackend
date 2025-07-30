19637719108f4665cdd5c8cc1d7bea610dab468e
ShikshaVerma-28 <133035276+ShikshaVerma-28@users.noreply.github.com>
2025-07-30 22:40:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 
 //Configure CORS
 const corsOptions = {
-    origin: '*', //Allow all origins (for development). For production, specify your domain
+origin: "https://kindle-frontend-mern.vercel.app/", //Allow all origins (for development). For production, specify your domain
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
